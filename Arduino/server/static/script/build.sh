ce27e0ec6c651efb7487756c1a555a5beceee2ab
damoyelang1992 <iqinfei@163.com>
2016-09-10 11:07:58 +0800

    index修改了upload和build接口信息一致，更改编译脚本，报告代码错误信息 modify the interface between upload and build,modify build.sh to report code error when build filed

--- Arduino/server/static/script/build.sh
+++ Arduino/server/static/script/build.sh
@@ -7,4 +7,6 @@ if [ 0 -eq $? ]
 then
 hexfiledir="180.76.179.148:5000/static/$1/.build/uno/firmware.hex"
 mosquitto_pub -h 180.76.179.148 -t $1 -m $hexfiledir 
+else 
+mosquitto_pub -h 180.76.179.148 -t $1 -m ‘Build failed,Looks like your code has a problem.’
 fi
